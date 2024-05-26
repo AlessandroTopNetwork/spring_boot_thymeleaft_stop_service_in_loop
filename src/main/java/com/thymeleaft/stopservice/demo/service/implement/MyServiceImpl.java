6341a24fb076a9d68aba228e33c5b7aac499fd72
AlessandroTopNetwork <>
2024-05-26 15:51:37 +0200

    add call autowired into method loop

--- src/main/java/com/thymeleaft/stopservice/demo/service/implement/MyServiceImpl.java
+++ src/main/java/com/thymeleaft/stopservice/demo/service/implement/MyServiceImpl.java
@@ -2,12 +2,17 @@ package com.thymeleaft.stopservice.demo.service.implement;
 
 import java.time.LocalTime;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.thymeleaft.stopservice.demo.service.MyService;
+import com.thymeleaft.stopservice.demo.util.Utility;
 
 @Service
 public class MyServiceImpl implements MyService{
+	
+	@Autowired
+	private Utility util;
 	
 	 private Boolean serviceRunning = true;
 
@@ -17,6 +22,7 @@ public class MyServiceImpl implements MyService{
 		while(serviceRunning) {
 			System.out.println(" ia m running now : " + LocalTime.now());
 			Thread.sleep(1000);
+			util.testMeServiceStoppedAutowired();
 		}
 	}
 
---
+++ src/main/java/com/thymeleaft/stopservice/demo/util/Utility.java
@@ -0,0 +1,12 @@
+package com.thymeleaft.stopservice.demo.util;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Utility {
+	
+	public void testMeServiceStoppedAutowired() {
+		System.out.println("ciao from utility");
+	}
+
+}
